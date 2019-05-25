1acd42bbdf9504dcba6306067eb7dbf9b8a7b317
wuyankun <wuyankun@lvmama.com>
2019-05-25 17:32:31 +0800

    二分

---
+++ src/main/java/com/wyk/arithmetic/sort/BinarySearch.java
@@ -0,0 +1,35 @@
+package com.wyk.arithmetic.sort;
+
+/**
+ * @author wuyankun
+ * @title: BinarySearch
+ * @projectName sort
+ * @description: TODO
+ * @date 2019/5/2517:14
+ */
+public class BinarySearch {
+
+    public static int binarySearch(int[] arr,int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end){
+            int mid = (end + start) / 2;
+
+            if (target == arr[mid]){
+                return mid;
+            }else if (target > arr[mid]){
+                start = mid + 1;
+            }else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 5, 8, 9};
+        System.out.println(binarySearch(arr, 2));
+    }
+
+}
